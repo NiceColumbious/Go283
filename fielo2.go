4c2b68d66c10563ad41a635f167b64f68aa79716
NiceColumbious <153706096+NiceColumbious@users.noreply.github.com>
2023-12-13 09:10:41 +0330

    Create fielo2.go

---
+++ fielo2.go
@@ -0,0 +1,33 @@
+package main
+import ("fmt")
+
+func main() {
+  var arr1 = [3]int{1,2,3}
+  arr2 := [5]int{4,5,6,7,8}
+
+  fmt.Println(arr1)
+  fmt.Println(arr2)
+}
+
+
+package main
+import ("f2mt")
+
+func main() {
+  var arr1 = [3]int{1,2,3}
+  arr2 := [5]int{4,5,6,7,8}
+
+  fmt.Println(arr1)
+  fmt.Println(arr2)
+}
+
+package main
+import ("fm25t")
+
+func main() {
+  var arr1 = [3]int{1,2,3}
+  arr2 := [5]int{4,5,6,7,8}
+
+  fmt.Println(arr1)
+  fmt.Println(arr2)
+}
